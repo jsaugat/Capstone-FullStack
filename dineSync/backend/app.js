c516586d8a25c0f77416f904b89202ef5cfbb3e7
Mr. Saugat <jsaugatt02@gmail.com>
2024-01-27 15:55:24 +0545

    utils

--- dineSync/backend/app.js
+++ dineSync/backend/app.js
@@ -1,37 +1,19 @@
 import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import router from "./src/routes/router.js";
-
-dotenv.config(); // can use process.env now
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
-const port = process.env.PORT || 6900;
 
-//? parse incoming requests with JSON payloads. and make it availble in request body (req.body)
-app.use(express.json());
-
-// GLOBAL middleware
-app.use("/", (req, res, next) => {
-  console.log(`- Request Method -> ${req.method}`);
-  console.log(`- Request Path -> ${req.url}`);
-  console.log(`- Request Body -> ${JSON.stringify(req.body)}`);
-  next();
-});
-
-// ROUTES middleware
-app.use("/app", router);
-
-//? Database connection
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("✅ SUCCESS :: MongoDB Connection");
-    // start listening once the db is connected...
-    app.listen(port, () => {
-      console.log(`👁️  WATCHING PORT : http://localhost:${port}`);
-    });
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
   })
-  .catch((error) => {
-    console.error("❌ FAILURE :: MongoDB Connection:", error);
-  });
+);
+// requests come from differenct sources like url, json, body/ form
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public")); // public directory in your project will be used to serve static files like pdf, images, html, css and other assets
+app.use(cookieParser()); // set access and CRUD operate on user cookies
+
+export { app };
